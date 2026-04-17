2deecaebbc384c4cf5453e77542ae9097dda524f
Rajesh Ganatra <rahil.rajesh-ganatra@capgemini.com>
2026-04-17 08:53:33 +0530

    init commit

--- src/ui/acme-logo.tsx
+++ src/ui/acme-logo.tsx
@@ -1,12 +1,12 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '@/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <GlobeAltIcon className="h-10 w-10 rotate-12" />
       <p className="text-[44px]">Acme</p>
     </div>
   );
